be283a64fce670436410ef7f5609186595d710d5
josephsmktel28 <joseph.satrio705@gmail.com>
2026-06-11 18:42:58 +0700

    Add ForceHttps middleware untuk production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
+        $middleware->append(\App\Http\Middleware\ForceHttps::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
